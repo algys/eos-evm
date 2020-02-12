4024e0eb9099c45c5d7cc7ff03a6e5d3ec658065
Algys Ievlev <algys.ievlev@gmail.com>
2020-02-12 21:15:09 +0300

    hello world works now

--- src/hello.cpp
+++ src/hello.cpp
@@ -52,11 +52,38 @@ public:
         raw_address[3] = 3;
         const eevm::Address to = eevm::from_big_endian(raw_address.data(), raw_address.size());
 
+        //Create sender address
+        raw_address[4] = 4;
+        const eevm::Address sender = eevm::from_big_endian(raw_address.data(), raw_address.size());
+
         // Create global state
         eevm::SimpleGlobalState gs;
 
         // Create contract
         const eevm::AccountState contract = gs.create(to, 0, code);
+
+        // Create transaction
+        eevm::NullLogHandler ignore;
+        eevm::Transaction tx(sender, ignore);
+
+        // Create processor
+        eevm::Processor p(gs);
+
+        // Execute code. All execution is associated with a transaction. This
+        // transaction is called by sender, executing the code in contract, with empty
+        // input (and no trace collection)
+        const eevm::ExecResult e = p.run(tx, sender, contract, {}, 0, nullptr);
+
+        // Check the response
+        if (e.er != eevm::ExitReason::returned) {
+            eosio::print_f("uexpected return code: %\n", (size_t)e.er);
+            return;
+        }
+
+        // Create string from response data, and print it
+        const std::string response(reinterpret_cast<const char*>(e.output.data()));
+
+        eosio::print_f("ok: %\n", response.data());
     }
 };
 
